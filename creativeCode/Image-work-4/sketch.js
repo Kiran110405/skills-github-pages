5a74fd2bc5b21902f72ecdd63496d98e4d3729c6
Kiran110405 <147168552+Kiran110405@users.noreply.github.com>
2024-05-27 01:01:36 +0100

    update

---
+++ creativeCode/Image-work-4/sketch.js
@@ -0,0 +1,21 @@
+
+
+let img;
+
+function preload(){
+  img = loadImage('assets/avengers.jpeg')
+}
+
+function setup() {
+  createCanvas(400, 400);
+  img.resize(windowWidth, 0);
+  createCanvas(img.width, img.height);
+  noLoop()
+}
+
+function draw() {
+  background(220);
+  image(img,0,0)
+     filter(THRESHOLD);
+  
+}
